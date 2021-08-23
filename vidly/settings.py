3dee590f9685d969b8ad624715bdf570efafa85b
NishantGupt786 <84934511+NishantGupt786@users.noreply.github.com>
2021-08-24 01:30:38 +0530

    Update settings.py

--- vidly/settings.py
+++ vidly/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'py&io@7x4v)-7!l9xv66w5wj=$b6m%99$$ikic&4k3jz*-9$v8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['peaceful-spire-10212.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,4 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
